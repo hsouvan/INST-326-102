2929798fd5e981960793f7e81c34159dd787e73b
hsouvan <HunterSouvan@gmail.com>
2026-04-10 13:19:52 -0400

    Added Function for CPU playing

--- spoons.py
+++ spoons.py
@@ -23,5 +23,30 @@ def swap_card (player, next_player):
   next_player.append(card)
   print(f"Player's Card Deck: {player}\nNext Player's Card Deck: {next_player}")
 
-# algorithm 4
+# algorithm 4 - Hunter
+def cpu_discard(cpu, next_player, cpu_hand, next_player_pile):
+    """
+    Allows the computer to decide what card to discard in Spoons.
+    
+    Parameters:
+        cpu (str): Name of the computer player
+        next_player (str): Name of the next player
+        cpu_hand (list of str): The computer's current hand
+        next_player_pile (list of str): The next players trash pile
 
+    Returns:
+        str or None: The card that was discarded, or None
+
+    Side Effects:
+        Removes the discarded card from cpu_hand
+        Adds the discarded card to the next_player_pile
+    """
+    for i in range(len(cpu_hand)):
+        for j in range(i + 1, len(cpu_hand)):
+            if cpu_hand[i][:-1] == cpu_hand[j][:-1]:
+                for card in cpu_hand:
+                    if card[:-1] != cpu_hand[i][:-1]:
+                        cpu_hand.remove(card)
+                        next_player_pile.append(card)
+                        return card
+    return None
